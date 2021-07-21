e3e28270450a6dfa8509671bc2637b09f79028e9
Mark Idleman <mark@replicahq.com>
2021-07-20 17:51:06 -0700

    fix after testing locally

--- setup_tests.sh
+++ setup_tests.sh
@@ -12,13 +12,11 @@ fi
 
 echo "Downloading OSM + GTFS data for mini_nor_cal test region"
 
-gsutil -m cp $MININORCAL_OSM_PATH ./web/test-data/mini_nor_cal.osm.pbf
+gsutil -m -o "GSUtil:parallel_process_count=1" cp $MININORCAL_OSM_PATH ./web/test-data/mini_nor_cal.osm.pbf
 mkdir ./web/test-data/gtfs/
-gsutil -m cp $MININORCAL_GTFS_PATH - | tar -C ./web/test-data/gtfs/ -xvf -
+gsutil -m -o "GSUtil:parallel_process_count=1" cp $MININORCAL_GTFS_PATH - | tar -C ./web/test-data/gtfs/ -xvf -
 sed -i -e "s/TEST_OSM/\/graphhopper\/web\/test-data\/mini_nor_cal.osm.pbf/g" ./test_gh_config.yaml
 export GTFS_FILE_LIST=$(ls ./web/test-data/gtfs/ | awk '{print "/graphhopper/web/test-data/gtfs/"$1}' | paste -s -d, -)
 sed -i -e "s/TEST_GTFS/${GTFS_FILE_LIST//\//\\/}/g" ./test_gh_config.yaml
 
 echo "Download successful! Test config test_gh_config.yaml can now be used"
-echo "printing contents of /web/test-data"
-ls -lar ./web/test-data
