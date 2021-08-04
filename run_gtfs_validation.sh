62fbf0d687a8a726fc3f3bebadaf3e344f2fc3f5
Mark Idleman <mark@replicahq.com>
2021-08-04 13:56:25 -0700

    try approach where gh action kicks off gcloud build :)

--- run_gtfs_validation.sh
+++ run_gtfs_validation.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -o errexit
 
+# Assumes that folder containing gtfs files is mounted as volume at /gtfs/
 export GTFS_FILE_LIST=$(ls ./gtfs/ | awk '{print "./gtfs/"$1}' | paste -s -d, -)
 java -Xmx16g -Ddw.graphhopper.gtfs.file=$GTFS_FILE_LIST \
   -Ddw.graphhopper.validation=true -classpath web/target/graphhopper-web-1.0-SNAPSHOT.jar \
