810227603ee7377c0a52827cbbd27f179efd9d6c
Mark Idleman <mark@replicahq.com>
2021-08-16 19:37:36 -0700

    try this

--- functional_test.sh
+++ functional_test.sh
@@ -25,12 +25,8 @@ docker run \
     -v "$TMPDIR:/graphhopper/transit_data/"\
     --rm \
      "$DOCKER_IMAGE_TAG" \
-     /bin/bash -c
-     "mkdir /graphhopper/test-data &&
-     cp -r /graphhopper/web/test-data /graphhopper/test-data &&
-     java -Xmx2g -Xms1g -XX:+UseG1GC -XX:MetaspaceSize=100M
-     -classpath web/target/graphhopper-web-1.0-SNAPSHOT.jar
-     -server com.graphhopper.http.GraphHopperApplication import test_gh_config.yaml"
+     /bin/bash -c "cp -r ./web/test-data . && java -Xmx2g -Xms1g -XX:+UseG1GC -XX:MetaspaceSize=100M \
+     -classpath web/target/graphhopper-web-1.0-SNAPSHOT.jar -server com.graphhopper.http.GraphHopperApplication import test_gh_config.yaml"
 
 # Run link-mapping step
 docker run \
