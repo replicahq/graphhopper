f6857168ef00cfb0e6b1153bffc7bec9a03dc53e
Bryan Williams <bryan@replicahq.com>
2022-10-24 17:18:17 -0400

    Remove custom speeds work

--- web-bundle/src/main/java/com/graphhopper/replica/ReplicaFlagEncoderFactory.java
+++ web-bundle/src/main/java/com/graphhopper/replica/ReplicaFlagEncoderFactory.java
@@ -1,35 +1,18 @@
 package com.graphhopper.replica;
 
 import com.graphhopper.http.TruckFlagEncoder;
-import com.graphhopper.http.TruckTagParser;
-import com.graphhopper.routing.ev.*;
 import com.graphhopper.routing.util.DefaultFlagEncoderFactory;
 import com.graphhopper.routing.util.FlagEncoder;
-import com.graphhopper.routing.util.VehicleEncodedValues;
 import com.graphhopper.util.PMap;
 
-import java.util.Set;
-
-import static com.graphhopper.routing.util.EncodingManager.getKey;
-
 public class ReplicaFlagEncoderFactory extends DefaultFlagEncoderFactory {
-    private final Set<String> vehicleNamesWithCustomSpeeds;
-
-    public ReplicaFlagEncoderFactory(Set<String> vehicleNamesWithCustomSpeeds) {
-        this.vehicleNamesWithCustomSpeeds = vehicleNamesWithCustomSpeeds;
-    }
 
     // "configuration" contains values from the flag encoder field in GraphHopper config (e.g.
     // graph.flag_encoders: car|turn_costs=true). we don't need to alter this field across different config files, so we
     // instead use this class to apply the necessary customizations to the default flag encoder
     @Override
     public FlagEncoder createFlagEncoder(final String name, PMap configuration) {
-        if (vehicleNamesWithCustomSpeeds.contains(name)) {
-            // vehicles with custom speeds use nonstandard vehicle names which must be added to the config for the GH
-            // internals to tolerate it. then we can delegate to the default car flag encoder
-            PMap customSpeedsConfig = new PMap(configuration).putObject("name", name);
-            return VehicleEncodedValues.car(customSpeedsConfig);
-        } else if (name.equals(TruckFlagEncoder.TRUCK_VEHICLE_NAME)) {
+        if (name.equals(TruckFlagEncoder.TRUCK_VEHICLE_NAME)) {
             return TruckFlagEncoder.createTruckFlagEncoder();
         }
 
--- web-bundle/src/main/java/com/graphhopper/replica/ReplicaVehicleTagParserFactory.java
+++ web-bundle/src/main/java/com/graphhopper/replica/ReplicaVehicleTagParserFactory.java
@@ -18,8 +18,6 @@
 
 package com.graphhopper.replica;
 
-import com.google.common.collect.ImmutableMap;
-import com.graphhopper.customspeeds.CustomSpeedsUtils;
 import com.graphhopper.http.TruckFlagEncoder;
 import com.graphhopper.http.TruckTagParser;
 import com.graphhopper.routing.ev.EncodedValueLookup;
@@ -27,38 +25,10 @@ import com.graphhopper.routing.util.DefaultVehicleTagParserFactory;
 import com.graphhopper.routing.util.VehicleTagParser;
 import com.graphhopper.util.PMap;
 
-import java.io.File;
-import java.io.IOException;
-import java.util.Map;
-
 public class ReplicaVehicleTagParserFactory extends DefaultVehicleTagParserFactory {
-    private final Map<String, File> vehicleNameToCustomSpeedFile;
-
-    public ReplicaVehicleTagParserFactory(Map<String, File> vehicleNameToCustomSpeedFile) {
-        this.vehicleNameToCustomSpeedFile = vehicleNameToCustomSpeedFile;
-    }
-
     @Override
     public VehicleTagParser createParser(EncodedValueLookup lookup, String name, PMap configuration) {
-        if (vehicleNameToCustomSpeedFile.containsKey(name)) {
-            // vehicles with custom speeds use nonstandard vehicle names which must be added to the config for the GH
-            // internals to tolerate it
-            configuration.putObject("name", name);
-            File customSpeedFile = vehicleNameToCustomSpeedFile.get(name);
-
-            try {
-                // we read the custom speeds mapping into memory so it can be efficiently applied during OSM import. all
-                // custom speed mappings will be simultaneously held in memory, but each custom speed file should be
-                // <= 250 MB (nationwide speed mapping file is 235MB)
-                ImmutableMap<Long, Double> osmWayIdToMaxSpeed =
-                        CustomSpeedsUtils.parseOsmWayIdToMaxSpeed(customSpeedFile);
-                return new ReplicaCustomSpeedsCarTagParser(lookup, configuration, osmWayIdToMaxSpeed);
-            } catch (IOException e) {
-                throw new RuntimeException("Failed to parse custom speed file at path "
-                        + customSpeedFile.getAbsolutePath() + ". Please ensure file exists and is in the correct " +
-                        "format!", e);
-            }
-        } else if (name.equals(TruckFlagEncoder.TRUCK_VEHICLE_NAME)) {
+        if (name.equals(TruckFlagEncoder.TRUCK_VEHICLE_NAME)) {
             configuration.putObject("block_fords", false);
             return TruckTagParser.createTruck(lookup, configuration);
         }
