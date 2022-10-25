f2887eaf584a406e29ba937811a3071e4a664802
Bryan Williams <bryan@replicahq.com>
2022-10-25 12:34:40 -0400

    CSV parsing fixes

--- replica-common/src/main/java/com/graphhopper/customspeeds/CustomSpeedsUtils.java
+++ replica-common/src/main/java/com/graphhopper/customspeeds/CustomSpeedsUtils.java
@@ -3,6 +3,7 @@ package com.graphhopper.customspeeds;
 import com.google.common.collect.ImmutableMap;
 import com.graphhopper.config.Profile;
 import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVParser;
 import org.apache.commons.csv.CSVRecord;
 
 import java.io.File;
@@ -16,7 +17,7 @@ import java.util.Map;
 public class CustomSpeedsUtils {
 
     public static final String CUSTOM_SPEED_FILE_CONFIG_FIELD = "custom_speed_file";
-    private static final String CUSTOM_SPEED_OSM_WAY_ID_COL_NAME = "osm_id";
+    private static final String CUSTOM_SPEED_OSM_WAY_ID_COL_NAME = "osm_way_id";
     private static final String CUSTOM_SPEED_MAX_SPEED_KPH_COL_NAME = "max_speed_kph";
 
     public static Map<String, File> getVehicleNameToCustomSpeedFile(List<Profile> profiles) {
@@ -45,12 +46,13 @@ public class CustomSpeedsUtils {
         ImmutableMap.Builder<Long, Double> osmWayIdToMaxSpeed = ImmutableMap.builder();
 
         Reader in = new FileReader(customSpeedFile);
-        Iterable<CSVRecord> records = CSVFormat.Builder.create().setHeader().build().parse(in);
-        for (CSVRecord record : records) {
+        CSVParser parser = CSVFormat.Builder.create().setHeader().build().parse(in);
+        for (CSVRecord record : parser) {
             Long osmWayId = Long.parseLong(record.get(CUSTOM_SPEED_OSM_WAY_ID_COL_NAME));
             Double maxSpeed = Double.parseDouble(record.get(CUSTOM_SPEED_MAX_SPEED_KPH_COL_NAME));
             osmWayIdToMaxSpeed.put(osmWayId, maxSpeed);
         }
+        parser.close();
 
         return osmWayIdToMaxSpeed.build();
     }
