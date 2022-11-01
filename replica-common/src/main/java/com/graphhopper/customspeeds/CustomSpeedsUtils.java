e0c40fcc87535257d5272c44e68b81655e2627e5
Bryan Williams <bryan@replicahq.com>
2022-11-01 13:09:45 -0500

    CR fixes, add test

--- replica-common/src/main/java/com/graphhopper/customspeeds/CustomSpeedsUtils.java
+++ replica-common/src/main/java/com/graphhopper/customspeeds/CustomSpeedsUtils.java
@@ -1,8 +1,6 @@
 package com.graphhopper.customspeeds;
 
 import com.google.common.collect.ImmutableMap;
-import com.google.common.collect.Maps;
-import com.google.common.collect.MoreCollectors;
 import com.graphhopper.config.Profile;
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVParser;
@@ -15,6 +13,7 @@ import java.io.Reader;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 
 public class CustomSpeedsUtils {
 
@@ -23,9 +22,14 @@ public class CustomSpeedsUtils {
     private static final String CUSTOM_SPEED_MAX_SPEED_KPH_COL_NAME = "max_speed_kph";
 
     /**
+     * Parses the custom speed files for any profiles using custom speeds. Each vehicle must be associated with the same
+     * custom speed file across all profiles.
+     *
      * @param profiles list of profiles for which the custom speed mapping should be retrieved
-     * @return map of vehicle name to mapping from OSM way id to the custom speed to use for that way, in kph. vehicles
+     * @return map of vehicle name to mapping from OSM way id to the custom speed to use for that way, in kph. Vehicles
      * without custom speeds are omitted from the map.
+     *
+     * @throws IllegalArgumentException if any profiles associate the same vehicle with different custom speed files
      * @throws RuntimeException if any custom speed files could not be found or are not properly formatted
      */
     public static ImmutableMap<String, ImmutableMap<Long, Double>> getVehicleNameToCustomSpeeds(List<Profile> profiles) {
@@ -38,25 +42,26 @@ public class CustomSpeedsUtils {
     }
 
     private static Map<String, File> getVehicleNameToCustomSpeedFile(List<Profile> profiles) {
-        Map<String, File> vehicleNameToCustomSpeedFileName = new HashMap<>();
+        Map<String, Optional<File>> vehicleNameToCustomSpeedFileName = new HashMap<>();
 
         for (Profile profile : profiles) {
-            String customSpeedFilePath = profile.getHints().getString(CustomSpeedsUtils.CUSTOM_SPEED_FILE_CONFIG_FIELD, "");
-            if (customSpeedFilePath.isEmpty()) {
-                continue;
-            }
-            File customSpeedFile = new File(customSpeedFilePath);
+            String customSpeedFilePath =
+                    profile.getHints().getString(CustomSpeedsUtils.CUSTOM_SPEED_FILE_CONFIG_FIELD, "");
+            Optional<File> customSpeedFile = customSpeedFilePath.isEmpty() ?
+                    Optional.empty() : Optional.of(new File(customSpeedFilePath));
 
             String vehicle = profile.getVehicle();
-            File existingCustomSpeedFile = vehicleNameToCustomSpeedFileName.get(vehicle);
+            Optional<File> existingCustomSpeedFile = vehicleNameToCustomSpeedFileName.get(vehicle);
             if (existingCustomSpeedFile != null && !existingCustomSpeedFile.equals(customSpeedFile)) {
-                throw new IllegalArgumentException("Vehicle " + vehicle + " cannot be associated with multiple " +
-                        "custom speed files. Please use a unique vehicle name for each vehicle with custom speeds!");
+                throw new IllegalArgumentException("Vehicle " + vehicle + " must be consistently associated with the " +
+                        "same custom speed file. Please use a unique vehicle name for each vehicle with custom speeds!");
             }
             vehicleNameToCustomSpeedFileName.put(vehicle, customSpeedFile);
         }
 
-        return ImmutableMap.copyOf(vehicleNameToCustomSpeedFileName);
+        return vehicleNameToCustomSpeedFileName.entrySet().stream()
+                .filter(entry -> entry.getValue().isPresent())
+                .collect(ImmutableMap.toImmutableMap(Map.Entry::getKey, entry -> entry.getValue().get()));
     }
 
     private static ImmutableMap<Long, Double> parseOsmWayIdToMaxSpeed(File customSpeedFile) {
@@ -64,6 +69,8 @@ public class CustomSpeedsUtils {
 
         try {
             Reader in = new FileReader(customSpeedFile);
+            // setHeader() with no args allows the column names to be automatically parsed from the first line of the
+            // file
             CSVParser parser = CSVFormat.Builder.create().setHeader().build().parse(in);
             for (CSVRecord record : parser) {
                 Long osmWayId = Long.parseLong(record.get(CUSTOM_SPEED_OSM_WAY_ID_COL_NAME));
