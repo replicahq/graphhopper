6c899f529f622c74a65e71ebb3a3abbb029e37e7
Mark Idleman <mark@replicahq.com>
2023-05-01 18:06:05 -0700

    cleanup again

--- replica-common/src/main/java/com/graphhopper/CustomGraphHopperGtfs.java
+++ replica-common/src/main/java/com/graphhopper/CustomGraphHopperGtfs.java
@@ -73,7 +73,7 @@ public class CustomGraphHopperGtfs extends GraphHopperGtfs {
 
         // Error if gtfs_link_mapper profile wasn't properly included in GH config (link mapper step will fail in this case)
         if (ghConfig.getProfiles().stream().noneMatch(p -> p.getName().equals(GTFS_LINK_MAPPER_PROFILE))) {
-            throw new RuntimeException("Graphhopper config must include " + GTFS_LINK_MAPPER_PROFILE + "listed as a profile!");
+            throw new RuntimeException("Graphhopper config must include " + GTFS_LINK_MAPPER_PROFILE + " listed as a profile!");
         }
         if (ghConfig.getCHProfiles().stream().noneMatch(p -> p.getProfile().equals(GTFS_LINK_MAPPER_PROFILE))) {
             throw new RuntimeException("Graphhopper config must include " + GTFS_LINK_MAPPER_PROFILE + " listed as a CH profile!");
