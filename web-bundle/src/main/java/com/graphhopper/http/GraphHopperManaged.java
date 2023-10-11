139921541d25add5aa4984b92c02727f132bb68d
Mark Idleman <mark@replicahq.com>
2023-10-11 11:43:28 -0700

    bump to latest commit w/ fixed transfer walking legs + exception fixes

--- web-bundle/src/main/java/com/graphhopper/http/GraphHopperManaged.java
+++ web-bundle/src/main/java/com/graphhopper/http/GraphHopperManaged.java
@@ -76,7 +76,7 @@ public class GraphHopperManaged implements Managed {
     }
 
     // MUST UPDATE EVERY TIME A "GRAPHHOPPER CORE UPDATE" OCCURS
-    // Copied directly from GraphHopperManaged.java on commit dd49e5685e3c2826e46f4d65ad4505211f35319d
+    // Copied directly from GraphHopperManaged.java on commit 79e4d23bdbf01bf53ed2f1f3387427fbaac53502
     public static List<Profile> resolveCustomModelFiles(String customModelFolder, List<Profile> profiles) {
         ObjectMapper jsonOM = Jackson.newObjectMapper();
         List<Profile> newProfiles = new ArrayList<>();
