f86186019e52cbe4814937881d9eaaa0804be979
Bryan Williams <bryan@replicahq.com>
2023-11-06 12:18:57 -0500

    Bump gtfs validation heap size for n1-standard-32 machines

--- run_gtfs_validation.sh
+++ run_gtfs_validation.sh
@@ -3,6 +3,6 @@ set -o errexit
 
 # Assumes that folder containing gtfs files is mounted as volume at /gtfs/
 export GTFS_FILE_LIST=$(ls ./gtfs/ | awk '{print "./gtfs/"$1}' | paste -s -d, -)
-java -Xmx58g -Ddw.graphhopper.gtfs.file=$GTFS_FILE_LIST \
+java -Xmx116g -Ddw.graphhopper.gtfs.file=$GTFS_FILE_LIST \
   -Ddw.graphhopper.validation=true -classpath web/target/graphhopper-web-1.0-SNAPSHOT.jar \
   com.graphhopper.http.GraphHopperApplication validate-gtfs ./configs/gtfs_validation_gh_config.yaml
