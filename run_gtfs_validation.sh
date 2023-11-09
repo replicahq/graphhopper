db65db1ec1d3ba4034f89d681476fd9b09404e71
Mark Idleman <mark@replicahq.com>
2023-11-09 12:24:21 -0800

    first pass at actually turning on trip-based PT router for GTFS validator (passing in GTFS schedule days)

--- run_gtfs_validation.sh
+++ run_gtfs_validation.sh
@@ -3,6 +3,11 @@ set -o errexit
 
 # Assumes that folder containing gtfs files is mounted as volume at /gtfs/
 export GTFS_FILE_LIST=$(ls ./gtfs/ | awk '{print "./gtfs/"$1}' | paste -s -d, -)
+
+# Parse passed in GTFS schedule days string and substitute it for placeholder in validation GH config
+GTFS_SCHEDULE_DAYS="$1"
+sed -i -e "s/{{ GTFS_SCHEDULE_DAYS }}/${GTFS_SCHEDULE_DAYS}/g" ./configs/gtfs_validation_gh_config.yaml
+
 java -Xmx58g -Ddw.graphhopper.gtfs.file=$GTFS_FILE_LIST \
   -Ddw.graphhopper.validation=true -classpath web/target/graphhopper-web-1.0-SNAPSHOT.jar \
   com.graphhopper.http.GraphHopperApplication validate-gtfs ./configs/gtfs_validation_gh_config.yaml
